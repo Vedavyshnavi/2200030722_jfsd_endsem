e8f320b95bd5c8a4590933ca445453cde73c6d38
Vedavyshnavi <140415633+Vedavyshnavi@users.noreply.github.com>
2024-12-07 08:37:24 +0530

    Add files via upload

---
+++ springboot-customer-project/src/main/java/com/klef/jfsd/exam/service/CustomerServiceImpl.java
@@ -0,0 +1,20 @@
+package com.klef.jfsd.exam.service;
+
+import com.klef.jfsd.exam.model.Customer;
+import com.klef.jfsd.exam.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+}
